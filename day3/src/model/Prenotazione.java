5bd4ac40c66d4f52ffcc7a3cb9ffae664808e695
cmarotta <c.marotta@simnt.it>
2016-04-06 12:00:52 +0200

    classe model prenotazioni

---
+++ day3/src/model/Prenotazione.java
@@ -0,0 +1,100 @@
+package model;
+
+
+import java.util.Date;
+
+
+public class Prenotazione {
+
+	private long id;
+	
+	private String nominativo;
+	private Date giorno;
+	private String turno;
+	private int numeroPersone; 
+	private String email;
+	private String numeroTelefono;
+	private String note;
+	
+	private Boolean stato; //Null non lavorato, 1 approvato, 0 rifiutato;
+
+	
+	
+	
+	
+	
+	
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNominativo() {
+		return nominativo;
+	}
+
+	public void setNominativo(String nominativo) {
+		this.nominativo = nominativo;
+	}
+
+	public Date getGiorno() {
+		return giorno;
+	}
+
+	public void setGiorno(Date giorno) {
+		this.giorno = giorno;
+	}
+
+	public String getTurno() {
+		return turno;
+	}
+
+	public void setTurno(String turno) {
+		this.turno = turno;
+	}
+
+	public int getNumeroPersone() {
+		return numeroPersone;
+	}
+
+	public void setNumeroPersone(int numeroPersone) {
+		this.numeroPersone = numeroPersone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNumeroTelefono() {
+		return numeroTelefono;
+	}
+
+	public void setNumeroTelefono(String numeroTelefono) {
+		this.numeroTelefono = numeroTelefono;
+	}
+
+	public String getNote() {
+		return note;
+	}
+
+	public void setNote(String note) {
+		this.note = note;
+	}
+
+	public Boolean getStato() {
+		return stato;
+	}
+
+	public void setStato(Boolean stato) {
+		this.stato = stato;
+	}
+	
+}
